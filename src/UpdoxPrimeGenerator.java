9c8c27c4dd68a3925608872f35e21ab0d827e48a
Haemee Nabors <HaemeeNabors@gmail.com>
2022-04-19 16:27:54 -0700

    Refactor start()
    Create new displayTitle()
    Create getRange() to get user input
    Create ascendRange() and use in start() to modify range in place and to keep the getRange() modular

--- src/UpdoxPrimeGenerator.java
+++ src/UpdoxPrimeGenerator.java
@@ -1,17 +1,16 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class UpdoxPrimeGenerator implements PrimeNumberGenerator {
 
     public void start(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Updox Prime Generator");
-        System.out.println("----------------------------------------------");
-        System.out.println("Enter inclusive starting endpoint:");
-        int first = scanner.nextInt();
-        System.out.println("Enter inclusive ending endpoint:");
-        int second = scanner.nextInt();
-        generate(first, second);
+        displayTitle();
+
+        int[] range = getRange();
+        ascendRange(range);
+
+        generate(range[0], range[1]);
     }
 
     @Override
@@ -23,5 +22,28 @@ public class UpdoxPrimeGenerator implements PrimeNumberGenerator {
     public List<Integer> generate(int startingValue, int endingValue) {
         System.out.println(startingValue + " " + endingValue);
         return null;
+    }
+
+    //implementation
+    private void displayTitle(){
+        System.out.println("\n----------------Updox Prime Generator--------------");
+        System.out.println("Displays ordered list of primes from entered range.");
+        System.out.println("---------------------------------------------------\n");
+    }
+
+    private int[] getRange(){
+        Scanner scanner = new Scanner(System.in);
+        int[] range = new int[2];
+
+        System.out.println("Enter inclusive starting endpoint:");
+        range[0] = scanner.nextInt();
+        System.out.println("Enter inclusive ending endpoint:");
+        range[1] = scanner.nextInt();
+
+        return range;
+    }
+
+    private void ascendRange(int[] range){
+        Arrays.sort(range);
     }
 }
