20a27cdc8d02b30cde7dafe70f688412f81ea67c
Haemee Nabors <HaemeeNabors@gmail.com>
2022-04-19 16:09:52 -0700

    Initial read of user input

--- src/UpdoxPrimeGenerator.java
+++ src/UpdoxPrimeGenerator.java
@@ -1,9 +1,17 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class UpdoxPrimeGenerator implements PrimeNumberGenerator {
 
     public void start(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Updox Prime Generator");
+        System.out.println("----------------------------------------------");
+        System.out.println("Enter inclusive starting endpoint:");
+        int first = scanner.nextInt();
+        System.out.println("Enter inclusive ending endpoint:");
+        int second = scanner.nextInt();
+        generate(first, second);
     }
 
     @Override
@@ -13,6 +21,7 @@ public class UpdoxPrimeGenerator implements PrimeNumberGenerator {
 
     @Override
     public List<Integer> generate(int startingValue, int endingValue) {
+        System.out.println(startingValue + " " + endingValue);
         return null;
     }
 }
